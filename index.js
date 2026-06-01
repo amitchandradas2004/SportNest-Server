db7f2687f052e98ddc590af9f60aa6f1c14ae921
amitchandradas2004 <amitcdas792@gmail.com>
2026-06-01 22:39:33 +0600

    Add Facility api is working

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
-
 const app = express();
-
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -19,15 +21,23 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("sportnest");
+    const facilityCollection = db.collection("facilities");
+
+    app.post("/facility", async (req, res) => {
+      const facilityData = req.body;
+      console.log(facilityData, "Facility data");
+      const result = await facilityCollection.insertOne(facilityData);
+      res.json(result);
+    });
+
     // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
