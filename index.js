95870879a2c493e439fce48c846af723385b563e
amitchandradas2004 <amitcdas792@gmail.com>
2026-06-02 02:12:38 +0600

    Dynamic api is working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const app = express();
@@ -30,6 +30,13 @@ async function run() {
       res.json(result);
     });
 
+    app.get("/facility/:id", async (req, res) => {
+      const { id } = await req.params;
+      const result = await facilityCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result);
+    });
     app.post("/facility", async (req, res) => {
       const facilityData = req.body;
       // console.log(facilityData, "Facility data");
